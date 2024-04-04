54d147d23998a2d9b78d458f1d6eed20519d82e6
Karastke <fkdlsaos1@naver.com>
2024-04-04 16:54:36 +0900

    feat: Add headlines
    페이지 헤드부분을 추가했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Porfile from "./assets/Photo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className='container m-auto px-4 py-6'>
+          <div className='flex justify-between items-center'>
+            <div>
+              <h1 className='font-bold text-xl'>Youngwook Portfolio</h1>
+            </div>
+            <div>
+              <ul className='flex gap-4'>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>Projects</a>
+                </li>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>Technologies</a>
+                </li>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>About me</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+
+        </div>
+      </header>
+      <main>
+        {/* 배너 부분 추가 예정 */ }
+        <section>
+          <div className='container m-auto px-4 py-10 flex gap-6'>
+            <div>
+              <h1 className='font-bold text-4xl'>Hello! I'm Youngwook</h1>
+              <h1 className='font-bold text-4xl mt-1 gradiant-text'>Full-Stack Developer</h1>
+              <p className='mt-4 text-gray-400'>Seeking to apply competent development skills with focus on collaboration, communication, and passion</p>
+            </div>
+            <div>
+              <img src={Porfile} width={220}></img>
+            </div>
+          </div>
+            
+
+        </section>
+      </main>
+
     </>
   )
 }
