dc5a39b31f8a99551d165cb5b3e337ac8cd7c0f3
Karastke <fkdlsaos1@naver.com>
2024-04-05 14:12:56 +0900

    feat: Add skills section
    기술스택 및 About me를 추가 했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Porfile from "./assets/Photo.png";
 import PorfileBg from "./assets/small.png";
 import Project1 from "./assets/project1.png";
 import Project2 from "./assets/project2.png";
+import Project3 from "./assets/project3.png";
+import Project4 from "./assets/project4.png";
 function App() {
 
   return (
@@ -41,18 +43,18 @@ function App() {
               <p className='mt-4 text-gray-400'>Seeking to apply competent development skills with focus on collaboration, communication, and passion</p>
             </div>
             <div className='relative'>
-              <img src={Porfile} width={420} className='relative z-10'/>
-              <img src={PorfileBg} className='absolute top-0 left-20 z-0'/>
+              <img src={Porfile} width={320} className='relative z-10'/>
+              <img src={PorfileBg} className='absolute top-0 left-10 z-0'/>
             </div>
           </div>
         </section>
         {/* Project section */}
         <section>
-          <div className='container m-auto px-4'>
+          <div className='container m-auto px-4 py-12'>
             <h2 className='text-2xl font-semibold '>Projects</h2>
             <div className='flex gap-10 mt-11'>
               <div className='border border-gray-500 rounded-md p-5'>
-                <img src={Project1} />
+                <img src={Project1} className='w-full h-auto'/>
                 <h3 className='text-2xl font-semibold mt-8'>가구 판매 페이지</h3>
                 <p className='text-gray-400 text-sm mt-2'>Responsive HTML/CSS 레이아웃을 이용한 온라인 가구샵 홈페이지입니다.</p>
                 <div className='flex justify-evenly mt-12'>
@@ -61,7 +63,27 @@ function App() {
                 </div>
               </div>
               <div className='border border-gray-500 rounded-md p-5'>
-                <img src={Project2} />
+                <img src={Project2} className='w-full h-auto'/>
+                <h3 className='text-2xl font-semibold mt-8'>가구 판매 페이지</h3>
+                <p className='text-gray-400 text-sm mt-2'>Responsive HTML/CSS 레이아웃을 이용한 온라인 가구샵 홈페이지입니다.</p>
+                <div className='flex justify-evenly mt-12'>
+                  <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Live preview</button>
+                  <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500'>Checkout github</button>
+                </div>
+              </div>
+            </div>
+            <div className='flex gap-10 mt-11'>
+              <div className='border border-gray-500 rounded-md p-5'>
+                <img src={Project1} className='w-full h-auto'/>
+                <h3 className='text-2xl font-semibold mt-8'>가구 판매 페이지</h3>
+                <p className='text-gray-400 text-sm mt-2'>Responsive HTML/CSS 레이아웃을 이용한 온라인 가구샵 홈페이지입니다.</p>
+                <div className='flex justify-evenly mt-12'>
+                  <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Live preview</button>
+                  <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500'>Checkout github</button>
+                </div>
+              </div>
+              <div className='border border-gray-500 rounded-md p-5'>
+                <img src={Project2} className='w-full h-auto'/>
                 <h3 className='text-2xl font-semibold mt-8'>가구 판매 페이지</h3>
                 <p className='text-gray-400 text-sm mt-2'>Responsive HTML/CSS 레이아웃을 이용한 온라인 가구샵 홈페이지입니다.</p>
                 <div className='flex justify-evenly mt-12'>
@@ -71,9 +93,77 @@ function App() {
               </div>
             </div>
           </div>
+           {/* 기술 섹션 작성하기*/}
+          <section className='py-10'>
+           <div className='container m-auto px-4'>
+            <h2 className='text-4xl font-semibold'>Technologies</h2>
+            <div className='mt-14'>
+                <div>
+                  <div className='flex justify-between items-center'> 
+                    <h2 className='text-lg font-semibold'>Unity(C#)</h2>
+                    <p className='text-gray-500'>Intermidate</p>
+                  </div>        {/* 퍼센티지 조정하는거 = w-[50%] */}
+                  <span className='w-[50%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md'/>
+                </div>
+                <div className='mt-8'>
+                  <div className='flex justify-between items-center'> 
+                    <h2 className='text-lg font-semibold'>Javascript, React</h2>
+                    <p className='text-gray-500'>Advanced</p>
+                  </div>
+                  <span className='w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md'/>
+                </div>
+                <div className='mt-8'>
+                  <div className='flex justify-between items-center'> 
+                    <h2 className='text-lg font-semibold'>JAVA, Spring Boot</h2>
+                    <p className='text-gray-500'>Advanced</p>
+                  </div>
+                  <span className='w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md'/>
+                </div>
+             </div>
+           </div> 
+          </section>
+          {/* 추가 기술 기재 */}
+          <section>
+            <div className='container m-auto px-4 py-14'>
+            <h2 className='text-3xl font-semibold'>Additional Technologies and Skills</h2>
+            <div className='text-xl flex justify-between mt-12 w-[80%]'>
+              <div>
+                <p className='font-semibold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git </p>
+              </div>
+              <div>
+              <p className='font-semibold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>English </p>
+              </div>
+              <div>
+              <p className='font-semibold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Teamwork </p> 
+              </div>
+              <div>
+              <p className='font-semibold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Teamwork </p> 
+              </div>
+            </div>
+            <div className='text-xl flex justify-between mt-8 w-[80%]'>
+              <div>
+                <p className='font-semibold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Git </p>
+              </div>
+              <div>
+              <p className='font-semibold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>English </p>
+              </div>
+              <div>
+              <p className='font-semibold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-5 before:absolute relative left-3'>Teamwork </p> 
+              </div>
+            </div>
+            </div>
+          </section>
+          <section className='py-8'>
+            <div className='container m-auto px-4'>
+                <h2 className='text-2xl font-semibold'>About me</h2>
+                <div className='mt-12'>
+
+
+                </div>
+            </div>
+          </section>
         </section>
       </main>
-
     </>
   )
 }
